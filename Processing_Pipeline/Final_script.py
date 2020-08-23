cc40dcf2445af15cb968ad0a7269e4a6f788df9c
RahulReady <52625075+RahulReady@users.noreply.github.com>
2020-08-22 21:45:42 -0500

    Baseline Processing File Setup

---
+++ Processing_Pipeline/Final_script.py
@@ -0,0 +1,5 @@
+import os
+import sys
+#TODO
+# [] Figure out importing and reorg project structure
+# https://stackoverflow.com/questions/37233140/python-module-not-found
---
+++ Processing_Pipeline/NLP/NLP_script.py
@@ -0,0 +1,12 @@
+import pandas as pd
+# TODO
+# [] Baseline script
+# [] NLP stuff
+
+# Chromedriver setup
+def nlp_analysis(df):
+    # do nlp stuff here. Maybe split into different functions?
+
+
+
+    if __name__ == "__main__":
