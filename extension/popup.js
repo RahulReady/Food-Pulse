f35d1f8f898f05f9ed2acedcd1b14127aafc1e86
rdasika <rohandasika@gmail.com>
2020-09-24 09:04:22 -0500

    extension grabbing URL properly

--- extension/popup.js
+++ extension/popup.js
@@ -5,18 +5,27 @@ document.addEventListener("DOMContentLoaded", function() {
 
 function popup() {
   chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+    // Get the currently active tab
     var activeTab = tabs[0];
+    // Encore URL so that all special characters are escaped
+    var url = encodeURI(activeTab.url);
 
     console.log(activeTab.url);
+    console.log(url);
 
+    // Send GET request to AWS API Gateway
     $.ajax({
       method: "GET",
-      url: "https://86e6t1qx46.execute-api.us-east-2.amazonaws.com/v1/reviews?",
-      data: {"url": activeTab.url},
+      url: "https://86e6t1qx46.execute-api.us-east-2.amazonaws.com/v1/reviews",
+      data: 
+      {
+        "url": url
+      },
     })
     .done( (response) => {
       console.log(response);
       chrome.tabs.sendMessage(activeTab.id, response);
+      // Todo: add logic to display the response in the extension popup
     });
   
   });
