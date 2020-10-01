6a51fc32bde9755c3ef308ac1f3aa42a0fd7ff1c
RahulReady <52625075+RahulReady@users.noreply.github.com>
2020-09-30 19:44:31 -0500

    venv for nlp + reviews file + foodbase data

--- src/NLP_script.py
+++ src/NLP_script.py
@@ -1,16 +1,33 @@
 import pandas as pd
-# To do
-# [] Baseline script
-# [] NLP stuff
+import spacy
+import spacy.displacy as displacy
+
+nlp = spacy.load('en_core_web_lg')
+doc = nlp('I love to eat chicken nuggets, tacos, burger, pizza on a day to day basis in Chicago Illinois with Mark Zuckerburg')
+for ent in doc.ents:
+    print(ent.text, ent.start_char, ent.end_char, ent.label_)
 
 
-# Chromedriver setup
-# def nlp_analysis(df):
-#     # do nlp stuff here. Maybe split into different functions?
 
-def test10():
-    print('111')
-    return
-def test11():
-    print('222')
-    return
+
+
+
+
+# class NLP:
+#     #Fix after everything is done
+#     def __init__(self, df):
+#         self.df = df
+
+#     # def testing(self):
+#     #     return 'hello' + self.df
+
+# if __name__ == '__main__':
+
+
+#     # Starting as a df (Need to change later to get json data)
+#     df = pd.read_csv('./src/review_data/reviews.csv')
+#     print(df.head())
+
+#     #top_reviews = NLP(df)
+
+
