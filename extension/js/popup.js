cfa9f6de974df791388e873f1bb52bdac11728f5
RahulReady <52625075+RahulReady@users.noreply.github.com>
2020-12-22 10:04:42 -0600

    extension - only keep best foods

--- extension/js/popup.js
+++ extension/js/popup.js
@@ -1,35 +1,45 @@
-
-document.addEventListener("DOMContentLoaded", function() {
-  document.getElementById("reviews").addEventListener("click", popup);
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("test").addEventListener("click", popup);
 });
 
 // Initialize Tabs in Popup
-$(document).ready(function(){
-  $('.tabs').tabs();
+$(document).ready(function () {
+  $(".tabs").tabs();
 });
 
 function popup() {
-  chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-    // Get the currently active tab
-    var activeTab = tabs[0];
-    // Encore URL so that all special characters are escaped
-    var url = encodeURI(activeTab.url);
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    let activeTab = tabs[0].url;
 
-    console.log(activeTab.url);
-    console.log(url);
+    document.getElementById("before").innerHTML = activeTab;
+    url = encodeURI(activeTab);
+    document.getElementById("restaurantName").innerHTML = url;
+  });
+}
+
+function popup1() {
+  console.log("here");
+  // Only seems to work when app is 'deployed'?
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    // Get the currently active tab
+    let activeTab = tabs[0].url;
+    // Encore URL so that all special characters are escaped
+    // var url = encodeURI(activeTab.url);
+
+    // console.log(activeTab.url);
+    // console.log(url);
+    document.getElementById("restaurantName").innerHTML = url;
 
     // Send GET request to AWS API Gateway
-    $.ajax({
-      method: "GET",
-      url: "https://86e6t1qx46.execute-api.us-east-2.amazonaws.com/v1/reviews",
-      data: 
-      {
-        "url": url
-      },
-    })
-    .done( (response) => {
-      console.log(response)
-      // Todo: add logic to display the response in the extension popup
-    });
+    // $.ajax({
+    //   method: "GET",
+    //   url: "https://86e6t1qx46.execute-api.us-east-2.amazonaws.com/v1/reviews",
+    //   data: {
+    //     url: url,
+    //   },
+    // }).done((response) => {
+    //   console.log(response);
+    //   // Todo: add logic to display the response in the extension popup
+    // });
   });
 }
